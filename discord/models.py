273f274a7ee41b7cb7fbe01905511b6144c00d28
Corey <corey.hampton@student.awesomeinc.org>
2022-04-22 13:57:35 +0000

    Make endpoints for each table

--- discord/models.py
+++ discord/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 
+class User(AbstractUser):
+    def __str__(self):
+        return self.email 
 
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -17,36 +19,43 @@ class Category(models.Model):
 class Server(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    # created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    users = models.ManyToManyField(User)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    users = models.ManyToManyField(User, related_name="members")
 
     def __str__(self):
         return self.name
 
+
 class Channel(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    server = models.ForeignKey(Server, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    server = models.ForeignKey(Server, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE , null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 class Relationship(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    is_friend = models.BooleanField(default=False)
+    follower = models.ForeignKey(User, related_name='follower', on_delete=models.CASCADE, null=True, blank=True)
+    followed = models.ForeignKey(User, related_name='followed', on_delete=models.CASCADE, null=True, blank=True)
+    status = models.CharField(max_length=255, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.user.username
 
+
 class Message(models.Model):
     content = models.TextField()
-    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    parent = models.OneToOneField('self', on_delete=models.CASCADE, null=True, blank=True)
     sent_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.content
